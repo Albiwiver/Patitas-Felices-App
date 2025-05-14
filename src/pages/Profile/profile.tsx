a07d59792f0a8e39c62ed09848364796310db964
Albiwiver <albiwiber@gmail.com>
2025-05-14 03:44:33 +0100

    quinto commit

--- src/pages/Profile/profile.tsx
+++ src/pages/Profile/profile.tsx
@@ -5,7 +5,7 @@ export const Profile: React.FC = () => {
     <section className=" w-full relative px-4 py-2 h-auto lg:min-h-screen flex justify-center items-center lg:justify-start xl:ml-8 ">
       <ProfileForm />
       <img
-        className="hidden lg:absolute lg:block lg:w-1/2 lg:h-full lg:top-0 lg:right-0 lg:object-cover lg:object-center -z-10"
+        className="hidden lg:absolute lg:block lg:w-1/2 lg:h-full lg:top-0 lg:right-0 lg:object-cover lg:object-center -z-30"
         src="assets/images/profile-dog-fingerprints.svg"
       ></img>
     </section>
